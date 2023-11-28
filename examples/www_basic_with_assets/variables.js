c75e241ab5000b16fdc0babc776aea02676093d9
aggregate1166877 <noreply@frostoven.com>
2023-11-29 03:43:17 +1300

    Add example with assets.

---
+++ examples/www_basic_with_assets/variables.js
@@ -0,0 +1,9 @@
+const variables = {
+  siteName: 'YourSiteHere',
+  siteDescription: 'Your site description goes here.',
+  easterEgg: 'I SAW THE FACE OF GOD, AND IT WAS SQUARE',
+};
+
+module.exports = {
+  variables,
+};
